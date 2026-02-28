1a07ef33061f5d95757a32c43bc13737c43e235a
hiroki <eeg1412@gmail.com>
2026-03-01 00:07:51 +0900

    build

---
+++ server/front/assets/config-BxYY2VpQ.js
@@ -0,0 +1,1 @@
+import{g as t}from"./auth-B_aZ1mjV.js";function g(){return t.get("/config/site-settings")}function i(){return t.get("/config/game-settings")}export{i as a,g};
---
+++ server/front/assets/inventory-CBYETWR4.js
@@ -0,0 +1,1 @@
+import{g as e}from"./auth-B_aZ1mjV.js";function n(){return e.get("/inventory/my")}export{n as g};
