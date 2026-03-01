5fe772c913b3f571233906a52daec2d09cfa35c3
hiroki <eeg1412@gmail.com>
2026-03-01 21:34:06 +0900

    build

---
+++ server/front/assets/config-hs9VaYvh.js
@@ -0,0 +1,1 @@
+import{g as t}from"./request-BHojpryK.js";function g(){return t.get("/config/site-settings")}function i(){return t.get("/config/game-settings")}export{i as a,g};
---
+++ server/front/assets/inventory-BoXYvOAQ.js
@@ -0,0 +1,1 @@
+import{g as e}from"./request-BHojpryK.js";function n(){return e.get("/inventory/my")}export{n as g};
