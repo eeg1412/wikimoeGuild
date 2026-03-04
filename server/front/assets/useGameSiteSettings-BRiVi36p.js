effc832f9414a5a6f6d44b8b22965b6349754abf
hiroki <eeg1412@gmail.com>
2026-03-04 22:04:31 +0900

    build

---
+++ server/front/assets/useGameSiteSettings-BRiVi36p.js
@@ -0,0 +1,1 @@
+import{a as n}from"./config-BGmxo5Qa.js";import{r as s}from"./index-CR7it6St.js";const e=s({siteTitle:"",siteSubTitle:"",siteKeywords:"",siteUrl:""}),t=s(!1);function u(){async function i(){if(!t.value)try{const a=await n();Object.assign(e.value,a.data.data||{}),t.value=!0}catch{}}return{siteSettings:e,settingsLoaded:t,loadSiteSettings:i}}export{u};
