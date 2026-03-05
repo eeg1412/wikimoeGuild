30531795bc0b077c6583042aab03ea8ed6004d0b
hiroki <eeg1412@gmail.com>
2026-03-05 15:51:01 +0900

    build

---
+++ server/front/assets/inventory-CFP6GxSB.js
@@ -0,0 +1,1 @@
+import{g as e}from"./request-D54Q-YLZ.js";function n(){return e.get("/inventory/my")}export{n as g};
---
+++ server/front/assets/useGameSiteSettings-B4ORJwx8.js
@@ -0,0 +1,1 @@
+import{a as n}from"./config-BYfDncxH.js";import{r as s}from"./index-nESUg6PE.js";const e=s({siteTitle:"",siteSubTitle:"",siteKeywords:"",siteUrl:""}),t=s(!1);function u(){async function i(){if(!t.value)try{const a=await n();Object.assign(e.value,a.data.data||{}),t.value=!0}catch{}}return{siteSettings:e,settingsLoaded:t,loadSiteSettings:i}}export{u};
