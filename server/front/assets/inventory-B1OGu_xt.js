4a3d5c8eac2abe6d61c8ec22def09149c09bcf62
hiroki <eeg1412@gmail.com>
2026-03-07 21:55:43 +0900

    build

---
+++ server/front/assets/inventory-B1OGu_xt.js
@@ -0,0 +1,1 @@
+import{g as e}from"./request-BN1_Ua6c.js";function n(){return e.get("/inventory/my")}export{n as g};
---
+++ server/front/assets/useGameSiteSettings-DBah-zpz.js
@@ -0,0 +1,1 @@
+import{a as n}from"./config-C_1uxc0w.js";import{r as s}from"./index-B6Ip2oBJ.js";const e=s({siteTitle:"",siteSubTitle:"",siteKeywords:"",siteUrl:""}),t=s(!1);function u(){async function i(){if(!t.value)try{const a=await n();Object.assign(e.value,a.data.data||{}),t.value=!0}catch{}}return{siteSettings:e,settingsLoaded:t,loadSiteSettings:i}}export{u};
