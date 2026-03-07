78d4fb9417792b395241493ce13ccde05452bfde
hiroki <eeg1412@gmail.com>
2026-03-07 21:27:30 +0900

    build

---
+++ server/front/assets/useGameSiteSettings-C86lafdO.js
@@ -0,0 +1,1 @@
+import{a as n}from"./config-CkH-kadr.js";import{r as s}from"./index-C9WaqkhE.js";const e=s({siteTitle:"",siteSubTitle:"",siteKeywords:"",siteUrl:""}),t=s(!1);function u(){async function i(){if(!t.value)try{const a=await n();Object.assign(e.value,a.data.data||{}),t.value=!0}catch{}}return{siteSettings:e,settingsLoaded:t,loadSiteSettings:i}}export{u};
