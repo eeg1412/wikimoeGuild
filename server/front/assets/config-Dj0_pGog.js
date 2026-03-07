53a1e4b5d2ee71e61178e0d7052f7c6f51fcbc47
hiroki <eeg1412@gmail.com>
2026-03-07 19:59:02 +0900

    build

---
+++ server/front/assets/config-Dj0_pGog.js
@@ -0,0 +1,1 @@
+import{g as t}from"./request-DH3-nf1e.js";function g(){return t.get("/config/site-settings")}function i(){return t.get("/config/game-settings")}export{g as a,i as g};
---
+++ server/front/assets/inventory-DRDKCMi8.js
@@ -0,0 +1,1 @@
+import{g as e}from"./request-DH3-nf1e.js";function n(){return e.get("/inventory/my")}export{n as g};
