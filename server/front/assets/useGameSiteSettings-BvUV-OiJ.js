05a5d80c749c0418fefe91d239569af20b1021d8
hiroki <eeg1412@gmail.com>
2026-03-07 20:24:46 +0900

    build

---
+++ server/front/assets/useGameSiteSettings-BvUV-OiJ.js
@@ -0,0 +1,1 @@
+import{a as n}from"./config-CB8eQPe_.js";import{r as s}from"./index-DZjaTgF1.js";const e=s({siteTitle:"",siteSubTitle:"",siteKeywords:"",siteUrl:""}),t=s(!1);function u(){async function i(){if(!t.value)try{const a=await n();Object.assign(e.value,a.data.data||{}),t.value=!0}catch{}}return{siteSettings:e,settingsLoaded:t,loadSiteSettings:i}}export{u};
